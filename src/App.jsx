bf953c37dcaecbc66aff57dc9d0d8b10bb7364de
pankaj-gitcode <pankajaditya75@gmail.com>
2024-10-09 21:15:43 +0530

    installed vite gsap tailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import GSAPTimeline from './GSAPTimeline'
+
+export default function App(){
+
+  return(
+   <>
+    <GSAPTimeline/>
+    <p className="text-2xl font-bold">Hi</p>
+   </>
+  )
+}
+
---
+++ src/GSAPTimeline.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function GSAPTimeline() {
+  return (
+    <div className="font-bold text-2xl">GSTimeline</div>
+  )
+}
