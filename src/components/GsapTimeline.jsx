59a0877e81970d1cbb512edbf03b0b5958486916
pankaj-gitcode <pankajaditya75@gmail.com>
2024-10-12 11:00:25 +0530

    gsap_to_from_fromTo_timeline_added_control-button

--- src/components/GsapTimeline.jsx
+++ src/components/GsapTimeline.jsx
@@ -15,18 +15,31 @@ export default function GsapTimeline(){
     tl.to('#gsapTimeline', {
         y:250, duration:2, ease:'back.in'
     },
-    tl.to('#gsapTimeline', {x:250})
+    tl.to('#gsapTimeline', {
+        x:250, duration:3, scale:2, ease:'back.inOut', borderRadius:'100%'
+    })
 )
     
     }, [])
 
     return(<>
-        <div className='bg-slate-500 h-screen flex flex-col justify-center'>
-            
-            <div id='gsapTimeline'className='w-28 h-28 bg-yellow-500 rounded-xl flex items-center font-semibold pl-1'>
-                gsapTimeline
-            </div>
+        <div className='bg-slate-500  '>
+                {/* --------------- BUTTON ----------------- */}
+            <div className='bg-slate-500 flex items-center justify-center h-28'>
+            <button className='bg-indigo-500 rounded-lg p-3 active:scale-105'
+            onClick={()=>tl.paused()?tl.play():tl.pause()} >
+                Play/Pause
+            </button>
 
+            </div>
+                  {/* --------------- BOX ------------ */}
+                <div className='h-screen flex flex-col justify-center'>
+                    
+                    <div id='gsapTimeline'className='w-28 h-28 bg-yellow-500 rounded-xl flex items-center font-semibold pl-1'>
+                        gsapTimeline
+                    </div>
+
+                </div>
         </div>
     </>)
 }
