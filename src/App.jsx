14c2223f0d7995ec97d99887262ad4345cc4eead
pankaj-gitcode <pankajaditya75@gmail.com>
2024-10-13 21:48:45 +0530

    gsap_stagger_updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ const GsapTo = lazy(()=>import('./components/GsapTo'));
 const GsapFrom = lazy(()=>import('./components/GsapFrom'));
 const GsapFromTo = lazy(()=>import('./components/GsapFromTo'));
 const GsapTimeline = lazy(()=>import('./components/GsapTimeline'));
+const GsapStagger = lazy(()=>import('./components/GsapStagger'));
 
 export default function App(){
   return(<>
@@ -14,11 +15,12 @@ export default function App(){
       <BrowserRouter>
     
         <Routes>
+          <Route path='/' element={<MainApp/>} />
           <Route path='/gsapto' element={<GsapTo/>}  />
           <Route path='/gsapfrom' element={<GsapFrom/>} />
-          <Route path='/gsapfromto' element={<GsapFromTo />}  />
+          <Route path='/gsapfromto' element={<GsapFromTo />} />
           <Route path='/gsaptimeline' element={<GsapTimeline />} />
-          <Route path='/' element={<MainApp/>} />
+          <Route path='/gsapstagger' element={<GsapStagger />} />
         </Routes>
       </BrowserRouter>
     </Suspense>
---
+++ src/components/GsapStagger.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import gsap from 'gsap'
+import { useGSAP} from '@gsap/react'
+
+export default function GsapStagger(){
+
+    useGSAP(()=>{
+        gsap.to('.stagger-box', {
+            y:270,
+            repeat:-1,
+            yoyoEase:true,
+            duration:2,
+            ease: 'circ.inOut',
+            stagger:0.2
+        })
+    }, [])
+
+    return(<>
+        <div className='bg-slate-500 h-screen flex items-center justify-center'>
+
+            <div className='flex gap-3'>
+                <div className='flex justify-center items-center rounded-lg w-28 h-28 bg-indigo-200 font-semibold stagger-box'>
+                    BOX-1
+                </div>
+
+                <div className='flex justify-center items-center rounded-lg w-28 h-28 bg-indigo-300 font-semibold stagger-box'>
+                    BOX-2
+                </div>
+
+                <div className='flex justify-center items-center rounded-lg w-28 h-28 bg-indigo-300 font-semibold stagger-box'>
+                    BOX-3
+                </div>
+
+                <div className='flex justify-center items-center rounded-lg w-28 h-28 bg-indigo-500 font-semibold stagger-box'>
+                    BOX-4
+                </div>
+
+                <div className='flex justify-center items-center rounded-lg w-28 h-28 bg-indigo-600 font-semibold stagger-box'>
+                    BOX-5
+                </div>
+
+                <div className='flex justify-center items-center rounded-lg w-28 h-28 bg-indigo-700 font-semibold stagger-box'>
+                    BOX-6
+                </div>
+
+                <div className='flex justify-center items-center rounded-lg w-28 h-28 bg-indigo-800 font-semibold stagger-box'>
+                    BOX-7
+                </div>
+
+            </div>
+
+
+        </div>
+    </>
+    )
+}
