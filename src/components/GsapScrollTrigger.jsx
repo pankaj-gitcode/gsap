ab8a25370b560d7877f6c4480b838c9842285c76
pankaj-gitcode <pankajaditya75@gmail.com>
2024-10-15 15:51:43 +0530

    scrolltrigger start_px

--- src/components/GsapScrollTrigger.jsx
+++ src/components/GsapScrollTrigger.jsx
@@ -13,15 +13,19 @@ export default function GsapScrollTrigger(){
             duration:3,
             scrollTrigger:{
                 trigger:'#c',
-                toggleActions: "restart pause restart pause"
+                toggleActions: "restart pause restart pause",
+                markers:true,
+                start: 'top 500', // trigger top level (box) reaches 500px of viewport
+                end:'+=100'     // end after scrolling 100px beyond the start 
             }
         })
     }, [])
     return(<>
-        <div className='h-[200vh] bg-slate-500 flex flex-col gap-10 items-center justify-center '>
+        <div className='h-[200vh] bg-slate-500 flex flex-col gap-10 '>
             <div id="a" className='box w-28 h-28 rounded-xl bg-orange-500'>Box-A</div>
             <div id="b" className='box w-28 h-28 rounded-xl bg-indigo-500'>Box-B</div>
-            <div id="c" className='box w-28 h-28 rounded-xl bg-green-500'>Box-C</div>
+            <div id="c" className='box w-28 h-28 rounded-xl bg-green-500 m-[500px]'>Box-C</div>
         </div>
+        
     </>)
 }
