5c9a5496eb454fc07b3b1da6f04fcff9d9c03de0
pankaj-gitcode <pankajaditya75@gmail.com>
2024-10-15 16:05:07 +0530

    scrolltrigger scrub_ghost

--- src/components/GsapScrollTrigger.jsx
+++ src/components/GsapScrollTrigger.jsx
@@ -10,18 +10,34 @@ export default function GsapScrollTrigger(){
         gsap.to('#c', {
             x:400,
             rotation:360,
-            duration:3,
+            duration:2,
             scrollTrigger:{
                 trigger:'#c',
-                toggleActions: "restart pause restart pause",
+                toggleActions: "restart pause reverse pause",
                 markers:true,
                 start: 'top 500', // trigger top level (box) reaches 500px of viewport
-                end:'+=100'     // end after scrolling 100px beyond the start 
+                end:'+=100',     // end after scrolling 100px beyond the start 
+                scrub: 1.5
             }
         })
+
+        gsap.to('.ghost', {
+            x:400,
+            rotation:360,
+            duration:2,
+            scrollTrigger:{
+                trigger:'#c',
+                toggleActions: "restart pause reverse pause",
+                markers:true,
+                start: 'top 500', // trigger top level (box) reaches 500px of viewport
+                end:'+=100',     // end after scrolling 100px beyond the start 
+                scrub: true
+            }
+        })
+
     }, [])
     return(<>
-        <div className='h-[200vh] bg-slate-500 flex flex-col gap-10 '>
+        <div className='h-[200vh] bg-slate-200 flex flex-col gap-10 items-center justify-center '>
             <div id="a" className='box w-28 h-28 rounded-xl bg-orange-500'>Box-A</div>
             <div id="b" className='box w-28 h-28 rounded-xl bg-indigo-500'>Box-B</div>
             <div id="c" className='box w-28 h-28 rounded-xl bg-green-500 m-[500px]'>Box-C</div>
