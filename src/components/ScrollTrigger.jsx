0457dacc1b308c0205cb452c591747febec5cf8a
pankaj-gitcode <pankajaditya75@gmail.com>
2024-11-12 12:07:32 +0530

    cards created

--- src/components/ScrollTrigger.jsx
+++ src/components/ScrollTrigger.jsx
@@ -4,20 +4,24 @@ import { images } from '../utils/utilities'
 export default class ScrollTrigger extends Component {
   render() {
     return (
-      <div id='parent' className=' w-full bg-black'>
-        <div id='container' className='flex items-center justify-center w-full'>
-            <div id='card' className='bg-[#2E231D] w-[60vw] h-[60vh]'>
+      <div id='parent' className='h-[460vh] w-full bg-black'>
+        <div id='container' className='flex items-center justify-center  w-full'>
+            <div id='card' className='w-[60vw] flex items-center justify-center flex-col p-20 '>
                 {
                     images.map(elem=>
-                    <div key='elem'>
-                        <img src={elem} alt={elem} />
-                        <h1>The <br/>Algorithm</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, a? Nostrum, explicabo exercitationem animi ipsum sequi impedit, reprehenderit voluptatem eveniet minus beatae neque totam quis.</p>
+                    <div key='elem' className='w-[40vw] lg:w-[27vw] bg-[#2E231D] mt-2 flex flex-col items-center justify-center py-20 rounded-xl'>
+                        <img src={elem} alt={elem} className='h-28 aspect-square'/>
+
+                        <h1 className='text-[#ada29b] text-3xl text-center pt-2 font-thin'>The <span className='text-white font-bold '><br/>Algorithm</span></h1>
+                        
+                        <p className='text-xs p-5 text-white text-center leading-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil dolorem Lorem ipsum dolor sit amet consectetur..</p>
+
                     </div>
                     )
                 }
             </div>
         </div>
+        {/* <img src='/images/rgh.png' alt="" /> */}
       </div>
     )
   }
