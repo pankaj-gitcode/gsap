3e3d87749226b7be3a71d1b0a9eef4b6363b9775
pankaj-gitcode <pankajaditya75@gmail.com>
2024-11-13 09:30:24 +0530

    code optimised

--- src/components/ScrollTrigger.jsx
+++ src/components/ScrollTrigger.jsx
@@ -1,79 +1,21 @@
 import React from 'react'
 import { images } from '../utils/utilities'
+
 export default function ScrollTrigger(){
   return(<>
-    <div className='h-[400vh] bg-black flex flex-col items-center justify-center gap-2 '>
+    <div className='text-[#fff] flex items-center justify-center flex-col gap-3 pb-[25vh] lg:py-[18vh]'>
+      {
+        images.map((imgs,index)=>
+          <div key={index} className='bg-[#2E231D] w-[35vw] lg:w-[27vw] sticky top-[10vh] py-[10vh] px-[38px] flex flex-col items-center justify-center rounded-2xl'>
 
-        {/* ---------------- CARD-1 ------------------- */}
-      <div className='sticky top-20 bg-[#2E231D] flex flex-col items-center justify-center w-[36vw] lg:w-[22vw] py-10 px-5 rounded-xl'>
-          
-          <img src={images[0]} alt={images[0]} className='w-[180px] aspect-square'/>
-          <h1 className='text-[#ada29b] text-3xl font-thin text-center'>The 
-            <span className='text-white font-bold'><br/>Algorithm</span>
-          </h1>
-          <p className='text-[#ada29b] text-xs py-6 text-center'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt excepturi amet quas.</p>
-      </div>
-          {/* ---------------- CARD-2 ------------------- */}
-            <div className='sticky top-20 z-1 bg-[#2E231D] flex flex-col items-center justify-center w-[36vw] lg:w-[22vw] py-10 px-5 rounded-xl'>
-          
-          <img src={images[0]} alt={images[1]} className='w-[180px] aspect-square'/>
-          <h1 className='text-[#ada29b] text-3xl font-thin text-center'>The 
-            <span className='text-white font-bold'><br/>Algorithm</span>
-          </h1>
-          <p className='text-[#ada29b] text-xs py-6 text-center'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt excepturi amet quas.</p>
-      </div>
-
-          {/* ---------------- CARD-3 ------------------- */}
-            <div className='sticky top-20 z-2 bg-[#2E231D] flex flex-col items-center justify-center w-[36vw] lg:w-[22vw] py-10 px-5 rounded-xl'>
-          
-          <img src={images[0]} alt={images[2]} className='w-[180px] aspect-square'/>
-          <h1 className='text-[#ada29b] text-3xl font-thin text-center'>The 
-            <span className='text-white font-bold'><br/>Algorithm</span>
-          </h1>
-          <p className='text-[#ada29b] text-xs py-6 text-center'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt excepturi amet quas.</p>
-      </div>
-
-          {/* ---------------- CARD-4 ------------------- */}
-            <div className='sticky top-20 z-3 bg-[#2E231D] flex flex-col items-center justify-center w-[36vw] lg:w-[22vw] py-10 px-5 rounded-xl'>
-          
-          <img src={images[0]} alt={images[3]} className='w-[180px] aspect-square'/>
-          <h1 className='text-[#ada29b] text-3xl font-thin text-center'>The 
-            <span className='text-white font-bold'><br/>Algorithm</span>
-          </h1>
-          <p className='text-[#ada29b] text-xs py-6 text-center'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt excepturi amet quas.</p>
-      </div>
-
-          {/* ---------------- CARD-5 ------------------- */}
-            <div className='sticky top-20 z-4 bg-[#2E231D] flex flex-col items-center justify-center w-[36vw] lg:w-[22vw] py-10 px-5 rounded-xl'>
-          
-          <img src={images[0]} alt={images[4]} className='w-[180px] aspect-square'/>
-          <h1 className='text-[#ada29b] text-3xl font-thin text-center'>The 
-            <span className='text-white font-bold'><br/>Algorithm</span>
-          </h1>
-          <p className='text-[#ada29b] text-xs py-6 text-center'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt excepturi amet quas.</p>
-      </div>
-
-          {/* ---------------- CARD-6 ------------------- */}
-            <div className='sticky top 1 z-5 bg-[#2E231D] flex flex-col items-center justify-center w-[36vw] lg:w-[22vw] py-10 px-5 rounded-xl'>
-          
-          <img src={images[0]} alt={images[5]} className='w-[180px] aspect-square'/>
-          <h1 className='text-[#ada29b] text-3xl font-thin text-center'>The 
-            <span className='text-white font-bold'><br/>Algorithm</span>
-          </h1>
-          <p className='text-[#ada29b] text-xs py-6 text-center'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt excepturi amet quas.</p>
-      </div>
-
-
-
-
+            <img src={imgs} alt="images" className='aspect-square w-[20vw]'/>
+            <h1 className='text-[#ada29b] text-3xl font-thin text-center pt-2'>The 
+              <span className='font-bold text-[#fff]'><br/>Algorithm</span>
+            </h1>
+            <p className='text-center text-xs pt-3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis voluptatum neque iure.</p>
+          </div>
+        )
+      }
     </div>
   </>)
 }
-
-
-
-
-// TODO: 1. parent div
-// TODO: 2. container div
-// TODO: 3. Card div
-// TODO: 4. img, header & p tags div
